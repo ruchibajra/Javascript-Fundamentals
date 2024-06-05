b7aafba6c8283baae1025b52e0ffaaba2d996a5d
ruchibajra <ruchibajracharya8@gmail.com>
2024-06-05 22:09:17 +0545

    first commit

---
+++ first.js
@@ -0,0 +1,13 @@
+// objects in js
+
+const product = {
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 200,
+
+};
+
+console.log(product);
+console.log(typeof product); //object
+console.log(typeof product["title"]); //string
